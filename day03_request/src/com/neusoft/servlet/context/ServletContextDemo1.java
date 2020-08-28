8f4bc87e86046e062d9001c8908304e8bf611836
18724522011 <775270046@qq.com>
2020-08-28 19:46:06 +0800

    111

---
+++ day03_request/src/com/neusoft/servlet/context/ServletContextDemo1.java
@@ -0,0 +1,30 @@
+package com.neusoft.servlet.context;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/servletContextDemo1")
+public class ServletContextDemo1 extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        ServletContext context2 = this.getServletContext();
+        ServletContext context1 = req.getServletContext();
+        System.out.println(context1);
+        System.out.println(context2);
+
+
+        System.out.println(context1==context2);
+    }
+}
